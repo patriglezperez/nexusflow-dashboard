6aedfef8394605cf50f359130561faaaf11636d5
Patricia <patricia.gonzalez@pix4d.com>
2025-06-25 18:48:24 +0200

    router

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,21 @@ import './index.css';
 import { TaskProvider } from './contexts/TaskContext';
 import { UserProvider } from './contexts/UserContext'; 
 import { ProjectProvider } from './contexts/ProjectContext'; 
-import { AuthProvider } from './contexts/AuthContext'; // Importar AuthProvider
+import { AuthProvider } from './contexts/AuthContext';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <UserProvider>
-        <ProjectProvider>
-          <TaskProvider>
-            <App />
-          </TaskProvider>
-        </ProjectProvider>
-      </UserProvider>
-    </AuthProvider> 
+    <BrowserRouter> 
+      <AuthProvider>
+        <UserProvider>
+          <ProjectProvider>
+            <TaskProvider>
+              <App />
+            </TaskProvider>
+          </ProjectProvider>
+        </UserProvider>
+      </AuthProvider> 
+    </BrowserRouter>
   </React.StrictMode>,
 );
-
-
